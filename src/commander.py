208ecf07bb725b1ea7508b2dd35964736389d470
Daeho Choi <112150244+DaeHOHoHOHo@users.noreply.github.com>
2023-12-20 16:42:07 +0900

    Update commander.py

--- src/commander.py
+++ src/commander.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import os
+from urllib.parse import unquote
+import subprocess
+import sys
 
 AGENT_URL = 'http://localhost:5000'
 
@@ -32,10 +35,30 @@ def download_file(file_name, save_path):
                 file.write(chunk)
         print("\nDownload Complete")
     else:
-        print("\nDownload Failed:", response.text)
+        print(f"\nDownload Failed. Status Code: {response.status_code}, Response Text: {response.text}")
+
+
+def run_sysmon():
+    sysmon_path = 'Sysmon.exe'
+    command = f'"{sysmon_path}"'
+
+    # 현재 스크립트를 관리자 권한으로 다시 실행
+    if sys.platform == 'win32' and 'admin' in os.environ.get('USERNAME', '').lower():
+        subprocess.Popen(command, shell=True)
+        print("Sysmon이 실행되었습니다.")
+    else:
+        try:
+            # 현재 스크립트를 관리자 권한으로 다시 실행
+            params = f'{sys.executable} {os.path.realpath(__file__)}'
+            subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+            print("Sysmon이 다시 실행되었습니다.")
+        except Exception as e:
+            print(f"관리자 권한으로 다시 실행하는 동안 오류 발생: {e}")
 
 if __name__ == '__main__':
+    get_request()
     # 보낼 파일 이름
-    upload_file("C:/Users/dealu/OneDrive/바탕 화면/프로젝트/project 2-2/test.txt")
+    # upload_file("test.exe")
     # 가상환경 파일이름, 저장할 주소
-    download_file('virtual.txt', 'C:/Users/dealu/OneDrive/바탕 화면/프로젝트/project 2-2/virtual.txt')
+
+    download_file("..\\..\\..\\..\\..\\..\\..\\..\\..\\..\\..\\Windows\\System32\\winevt\\Logs\\Microsoft-Windows-Sysmon%4Operational.evtx", 'C:/Users/dealu/OneDrive/바탕 화면/프로젝트/project 2-2/test/sys_test.evtx')
